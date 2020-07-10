8653df610887776000d708851114f4a0ee025335
Yuri Alves Miguel <yuri.snp@hotmail.com>
2020-07-09 23:10:46 -0300

    validation

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -2,7 +2,7 @@
   <div class="conversor">
     <img alt="Vue logo" class="text-center" src="./../assets/logo.png" />
     <h2>{{ moedaA }} {{ to }} {{ moedaB }}</h2>
-    <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA" />
+    <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaAvalue" />
     <input type="button" value="Converter" v-on:click="converter" />
     <h2>{{moedaB_value}}</h2>
   </div>
@@ -16,6 +16,7 @@ export default {
   data() {
     return {
       to: "Para",
+      moedaAvalue: this.moedaA,
       moedaA_value: "",
       moedaB_value: 0
     };
@@ -23,29 +24,66 @@ export default {
 
   methods: {
     async converter() {
-      let from_to = `${this.moedaA}_${this.moedaB}`;
-      let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=3a1b8c53c13a6a68c088`;
-      let cloneMoedaA = this.moedaA;
-      let cloneMoedaB = this.moedaB;
+      if (this.moedaA_value == "") {
+        alert("Digite um valor");
+      } else {
+        let from_to = `${this.moedaA}_${this.moedaB}`;
+        let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=3a1b8c53c13a6a68c088`;
 
-      this.to = "Carregando ...";
-      this.moedaA = "";
-      this.moedaB = "";
+        let cloneMoedaA = this.moedaA;
+        let cloneMoedaB = this.moedaB;
 
-      await fetch(url)
-        .then(res => {
-          return res.json();
-        })
-        .then(json => {
-          let value = json[from_to];
-          this.moedaB_value = (value * parseFloat(this.moedaA_value)).toFixed(
-            2
-          );
+        this.to = "Carregando ...";
+        this.moedaA = "";
+        this.moedaB = "";
 
-          this.moedaA = cloneMoedaA;
-          this.moedaB = cloneMoedaB;
-          this.to = "Para";
-        });
+        await fetch(url)
+          .then(res => {
+            return res.json();
+          })
+          .then(json => {
+            let value = json[from_to];
+
+            switch (cloneMoedaA) {
+              case "USD":
+                this.moedaB_value =
+                  "R$ " + (value * parseFloat(this.moedaA_value)).toFixed(2);
+                break;
+
+              case "CAD":
+                this.moedaB_value =
+                  "R$ " + (value * parseFloat(this.moedaA_value)).toFixed(2);
+                break;
+
+              case "EUR":
+                this.moedaB_value =
+                  "R$ " + (value * parseFloat(this.moedaA_value)).toFixed(2);
+                break;
+            }
+
+            switch (cloneMoedaB) {
+              case "USD":
+                this.moedaB_value =
+                  "$ " + (value * parseFloat(this.moedaA_value)).toFixed(2);
+                break;
+
+              case "CAD":
+                this.moedaB_value =
+                  "C$ " + (value * parseFloat(this.moedaA_value)).toFixed(2);
+                break;
+
+              case "EUR":
+                this.moedaB_value =
+                  "€ " + (value * parseFloat(this.moedaA_value)).toFixed(2);
+                break;
+            }
+
+            this.moedaA = cloneMoedaA;
+            this.moedaB = cloneMoedaB;
+            this.to = "Para";
+            this.value_validation = "";
+          });
+      }
     }
   }
 };
